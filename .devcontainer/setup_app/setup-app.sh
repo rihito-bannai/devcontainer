b2869f5b4e6bde49ac5523a29bfc9f7911d1dde0
Rihito Bannai <rihitobannai@Rihitos-MacBook-Pro.local>
2022-05-25 13:34:35 +0900

    setup_projectディレクトリをsetup_appに変更し、"setup.sh"を"setup-app.sh"に変更

--- .devcontainer/setup_app/setup-app.sh
+++ .devcontainer/setup_app/setup-app.sh
@@ -1,6 +1,6 @@
 cd /home/workspace
 touch .gitignore
-cp /home/workspace/.devcontainer/setup_project/.gitignore /home/workspace/.gitignore
+cp /home/workspace/.devcontainer/setup_app/.gitignore /home/workspace/.gitignore
 poetry new argent
 cd argent
 mv README.rst README.md
@@ -9,15 +9,15 @@ cd argent
 poetry run django-admin startproject conf .
 poetry add environs[django]@9.5.0
 touch .env
-cp /home/workspace/.devcontainer/setup_project/.env /home/workspace/argent/argent/.env
+cp /home/workspace/.devcontainer/setup_app/.env /home/workspace/argent/argent/.env
 mkdir templates
 touch templates/base.html
-cp /home/workspace/.devcontainer/setup_project/base.html /home/workspace/argent/argent/templates/base.html
+cp /home/workspace/.devcontainer/setup_app/base.html /home/workspace/argent/argent/templates/base.html
 mkdir -p static/css
 mkdir static/js
 touch static/css/base.css
 touch static/js/base.js
-cp /home/workspace/.devcontainer/setup_project/settings.py /home/workspace/argent/argent/conf/settings.py
+cp /home/workspace/.devcontainer/setup_app/settings.py /home/workspace/argent/argent/conf/settings.py
 cd /home/workspace
 git init
 git config --local user.email "you@example.com"
