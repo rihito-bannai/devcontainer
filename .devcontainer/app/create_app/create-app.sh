bc980a6b4bc8a15fa0224750f9e6455b49bba4fe
YourGitName <your_git_email@example.com>
2022-05-25 14:54:14 +0000

    新規プロジェクト作成

--- .devcontainer/app/create_app/create-app.sh
+++ .devcontainer/app/create_app/create-app.sh
@@ -1,28 +1,28 @@
 cd /home/workspace
 touch .gitignore
-cp /home/workspace/.devcontainer/app/$1_app/.gitignore /home/workspace/.gitignore
-poetry new $2
-cd $2
+cp /home/workspace/.devcontainer/app/create_app/.gitignore /home/workspace/.gitignore
+poetry new $1
+cd $1
 mv README.rst README.md
-poetry add django@$3.$4
-cd $2
+poetry add django@$2.$3
+cd $1
 poetry run django-admin startproject conf .
-poetry add environs[django]@$5
+poetry add environs[django]@$4
 touch .env
-cp /home/workspace/.devcontainer/app/$1_app/.env /home/workspace/$2/$2/.env
+cp /home/workspace/.devcontainer/app/create_app/.env /home/workspace/$1/$1/.env
 mkdir templates
 touch templates/base.html
-cp /home/workspace/.devcontainer/app/$1_app/base.html /home/workspace/$2/$2/templates/base.html
+cp /home/workspace/.devcontainer/app/create_app/base.html /home/workspace/$1/$1/templates/base.html
 mkdir -p static/css
 mkdir static/js
 touch static/css/base.css
 touch static/js/base.js
-cp /home/workspace/.devcontainer/app/$1_app/django_$3/settings.py /home/workspace/$2/$2/conf/settings.py
+cp /home/workspace/.devcontainer/app/create_app/django_$2/settings.py /home/workspace/$1/$1/conf/settings.py
 cd /home/workspace
 git init
-git config --local user.email "$6"
-git config --local user.name "$7"
-git config --local core.editor "$8"
+git config --local user.email "$5"
+git config --local user.name "$6"
+git config --local core.editor "$7"
 git add -A
 git commit -m "新規プロジェクト作成"
 bash
