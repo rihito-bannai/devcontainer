4e04c064ad3f45e56c29d124e46ef5ce716711f8
Your Name <you@example.com>
2022-05-25 15:33:00 +0900

    ディレクトリ構造を変更

--- .devcontainer/app/create_app/create-app.sh
+++ .devcontainer/app/create_app/create-app.sh
@@ -1,6 +1,6 @@
 cd /home/workspace
 touch .gitignore
-cp /home/workspace/.devcontainer/create_app/.gitignore /home/workspace/.gitignore
+cp /home/workspace/.devcontainer/app/create_app/.gitignore /home/workspace/.gitignore
 poetry new argent
 cd argent
 mv README.rst README.md
@@ -9,15 +9,15 @@ cd argent
 poetry run django-admin startproject conf .
 poetry add environs[django]@9.5.0
 touch .env
-cp /home/workspace/.devcontainer/create_app/.env /home/workspace/argent/argent/.env
+cp /home/workspace/.devcontainer/app/create_app/.env /home/workspace/argent/argent/.env
 mkdir templates
 touch templates/base.html
-cp /home/workspace/.devcontainer/create_app/base.html /home/workspace/argent/argent/templates/base.html
+cp /home/workspace/.devcontainer/app/create_app/base.html /home/workspace/argent/argent/templates/base.html
 mkdir -p static/css
 mkdir static/js
 touch static/css/base.css
 touch static/js/base.js
-cp /home/workspace/.devcontainer/create_app/settings.py /home/workspace/argent/argent/conf/settings.py
+cp /home/workspace/.devcontainer/app/create_app/settings.py /home/workspace/argent/argent/conf/settings.py
 cd /home/workspace
 git init
 git config --local user.email "you@example.com"
