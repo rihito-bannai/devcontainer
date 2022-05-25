9200d5665d0b7d876cec98dbd920b23ad4a04350
Your Name <you@example.com>
2022-05-25 19:19:20 +0900

    一部の値を変数に置き換えた

--- .devcontainer/app/create_app/create-app.sh
+++ .devcontainer/app/create_app/create-app.sh
@@ -1,28 +1,28 @@
 cd /home/workspace
 touch .gitignore
-cp /home/workspace/.devcontainer/app/create_app/.gitignore /home/workspace/.gitignore
-poetry new argent
-cd argent
+cp /home/workspace/.devcontainer/app/$1_app/.gitignore /home/workspace/.gitignore
+poetry new $2
+cd $2
 mv README.rst README.md
-poetry add django@3.2.13
-cd argent
+poetry add django@$3.$4
+cd $2
 poetry run django-admin startproject conf .
-poetry add environs[django]@9.5.0
+poetry add environs[django]@$5
 touch .env
-cp /home/workspace/.devcontainer/app/create_app/.env /home/workspace/argent/argent/.env
+cp /home/workspace/.devcontainer/app/$1_app/.env /home/workspace/$2/$2/.env
 mkdir templates
 touch templates/base.html
-cp /home/workspace/.devcontainer/app/create_app/base.html /home/workspace/argent/argent/templates/base.html
+cp /home/workspace/.devcontainer/app/$1_app/base.html /home/workspace/$2/$2/templates/base.html
 mkdir -p static/css
 mkdir static/js
 touch static/css/base.css
 touch static/js/base.js
-cp /home/workspace/.devcontainer/app/create_app/django_3.2/settings.py /home/workspace/argent/argent/conf/settings.py
+cp /home/workspace/.devcontainer/app/$1_app/django_$3/settings.py /home/workspace/$2/$2/conf/settings.py
 cd /home/workspace
 git init
-git config --local user.email "you@example.com"
-git config --local user.name "Your Name"
-git config --local core.editor "code --wait"
+git config --local user.email "$6"
+git config --local user.name "$7"
+git config --local core.editor "$8"
 git add -A
-git commit -m "セットアップ"
+git commit -m "新規プロジェクト作成"
 bash
