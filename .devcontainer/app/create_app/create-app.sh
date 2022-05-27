f3e7d15b0d4ec39ee4fc1e4544bc99b1bfb517bf
Rihito Bannai <rihito.bannai@gmail.com>
2022-05-27 14:35:10 +0900

    Git情報を個人ごとに設定できるように変更

--- .devcontainer/app/create_app/create-app.sh
+++ .devcontainer/app/create_app/create-app.sh
@@ -21,9 +21,4 @@ touch static/js/base.js
 cp /home/workspace/.devcontainer/app/create_app/django_$2/settings.py /home/workspace/$1/$1/conf/settings.py
 cd /home/workspace
 git init
-git config --local user.email "$5"
-git config --local user.name "$6"
-git config --local core.editor "$7"
-git add -A
-git commit -m "新規プロジェクト作成"
 bash
